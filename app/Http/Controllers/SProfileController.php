a0efab31bec367344a1a8fee6666588907c33fe4
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-08-28 22:13:11 +0530

    make table new

---
+++ app/Http/Controllers/SProfileController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\SProfile;
+
+class SProfileController extends Controller
+{
+    //
+}
---
+++ database/migrations/2025_08_28_155627_create_s_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sprofiles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); // foreign key
+            $table->integer('Index_no');
+            $table->string('Faculty');
+        $table->string('Department');
+        $table->string('Address');
+        $table->string('Blood_Group');
+        $table->string('Medical_Condition');
+        $table->string('Telephone');
+        $table->timestamps();
+
+        $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('s_profiles');
+    }
+};
