97c76de0eae320e41a661bf01916021a1fbb7b96
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-08-30 23:53:41 +0530

    webpage

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\SProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,8 +10,21 @@ Route::get('/', function () {
 Route::get('/S_interface', function(){
     return view('S_interface');
 });
+Route::get('/profile/{id}', function($id){
+    $user = \App\Models\User::find($id);
+    return view('Profile', compact('user'));
+});
 
+// Show profile for the currently authenticated user (if logged in)
+Route::get('/profile', function () {
+    if (! auth()->check()) {
+        return redirect('/login');
+    }
 
+    $user = auth()->user();
+    return view('Profile', compact('user'));
+});
+Route::post('/update', [SProfileController::class, 'update'])->name('update');
 Route::get('/register', [UserController::class, 'showRegistrationForm']);
 Route::post('/register', [UserController::class, 'register'])->name('register');
 
