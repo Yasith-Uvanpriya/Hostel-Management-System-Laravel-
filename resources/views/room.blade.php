f537fc34ac669708f19e3ec384de7818532c4cb8
Buthmika <buthmikamenushan@gmail.com>
2025-09-10 11:22:52 +0530

    Add custom CSS to room view
    Included room.css in the room.blade.php template to enable custom styling for the room view.

--- resources/views/room.blade.php
+++ resources/views/room.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+     <link rel="stylesheet" href="{{ asset('css/room.css') }}">
 </head>
 <body>
     <div class="container mt-5">
@@ -136,7 +137,7 @@
                             bedDropdown.innerHTML += `<li><a class="dropdown-item bed-item" href="#" data-value="${i}">${i}</a></li>`;
                             lockerDropdown.innerHTML += `<li><a class="dropdown-item locker-item" href="#" data-value="${i}">${i}</a></li>`;
                         }
-                        
+
                         // Add event listeners to new bed and locker items
                         addBedLockerListeners();
                     });
